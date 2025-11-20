74d4fac6bfcc6e87763ff981293ef8783e0af256
LeThinkNow <maelcharca@gmail.com>
2025-11-20 14:11:11 -0500

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({ apiKey: 'AIzaSyB4KveIkzDsKqf55R0joh3kIpfm0BVlJHg' })],
+  plugins: [googleAI({ apiKey: 'AIzaSyB4KveIkzDsKqf55R0joh3kIpfm0BVlJ' })],
   model: 'googleai/gemini-2.5-flash',
 });
